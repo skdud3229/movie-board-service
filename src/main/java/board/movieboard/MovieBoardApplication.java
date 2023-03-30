5ac2991c31f2a86db4ffd342201a29b9d826a595
skdud3229 <nayoung4300@naver.com>
2023-03-30 19:18:11 +0900

    Initial commit

---
+++ src/main/java/board/movieboard/MovieBoardApplication.java
@@ -0,0 +1,13 @@
+package board.movieboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/board/movieboard/MovieBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package board.movieboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
