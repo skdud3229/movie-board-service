730c16a8e521a728c68c5af0bbcfd4dee72170d1
sohye0n <sbb0416@naver.com>
2023-04-03 00:40:08 +0900

    root character entity added

---
+++ src/main/java/board/movieboard/Entity/Character.java
@@ -0,0 +1,15 @@
+package board.movieboard.Entity;
+
+public class Character {
+    private int Characterid;
+
+    private String CharacterName;
+
+    private String Role;
+
+    private String RealName;
+
+    private String PhotoUrl;
+
+    private int Author;
+}
---
+++ src/main/java/board/movieboard/Entity/Root.java
@@ -0,0 +1,7 @@
+package board.movieboard.Entity;
+
+public class Root {
+    private int Rootid;
+
+    private String Label;
+}
