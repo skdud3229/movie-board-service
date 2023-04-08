f17167261674f20193d136acab8747e17de99e23
nayou <nayoung4300@naver.com>
2023-04-09 07:32:10 +0900

    FEAT:draw and add nodes drawing from controller and add nodes in client

--- src/main/java/com/movieworld/movieboard/controller/NetworkController.java
+++ src/main/java/com/movieworld/movieboard/controller/NetworkController.java
@@ -1,12 +1,31 @@
 package com.movieworld.movieboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class NetworkController {
     @GetMapping("/network")
-    String network(){
-        return "network";
+    String network(Model model){
+        Node node1=new Node("0",false, "minions_bob.jpg","bob","it's minions bob!");
+        Node node2=new Node("1",false,null,"second","this is second node");
+        Node node3=new Node("2",false,null,"third","this is third node");
+        List<Node> nodes=new ArrayList<>();
+        nodes.add(node1);
+        nodes.add(node2);
+        nodes.add(node3);
+        model.addAttribute("nodeList",nodes);
+        return "network_sample";
+    }
+    @PostMapping("/network")
+    String saveNetwork(@RequestBody List<Node> nodeList){
+        System.out.println(nodeList);
+        return "redirect:/network";
     }
 }
---
+++ src/main/java/com/movieworld/movieboard/controller/Node.java
@@ -0,0 +1,60 @@
+package com.movieworld.movieboard.controller;
+
+public class Node {
+    String id;
+    //long boardId;
+    boolean isHub;
+    String PhotoUrl;
+    String name;
+    String details;
+    //String author;
+    Node(String id, boolean isHub, String PhotoUrl, String name, String details){
+        this.id=id;
+        this.isHub=isHub;
+        this.PhotoUrl=PhotoUrl;
+        this.name=name;
+        this.details=details;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public boolean isHub() {
+        return isHub;
+    }
+
+    public void setHub(boolean hub) {
+        isHub = hub;
+    }
+
+    public String getPhotoUrl() {
+        return PhotoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        PhotoUrl = photoUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+}
